52c9ddb7cc80cf95d3f2c85851b9c22d9e070552
Adrienne Yao <adrienne.yao@icloud.com>
2019-01-09 16:44:08 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,6 +6,9 @@ class Scraper
   def self.scrape_index_page(index_url)
     doc = Nokogiri::HTML(open("index_url"))
 
+    doc.css(".student-card").each do |studentCard|
+    end
+
   end
 
   def self.scrape_profile_page(profile_url)
