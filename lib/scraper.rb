6d9c426d3e95be37dd3855e2838b79204824b08d
Adrienne Yao <adrienne.yao@icloud.com>
2019-01-09 16:50:07 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,7 +4,7 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    doc = Nokogiri::HTML(open("index_url"))
+    doc = Nokogiri::HTML(open(index_url))
 
     doc.css(".student-card").each do |studentCard|
       binding.pry
